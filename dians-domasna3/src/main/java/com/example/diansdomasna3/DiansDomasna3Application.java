337da67a7180ff5a7e9248748f9a4ce651f7b7c9
Leonco1 <leonid20021@gmail.com>
2023-12-17 18:40:29 +0100

    Renamed some files

--- dians-domasna3/src/main/java/com/example/diansdomasna3/DiansDomasna3Application.java
+++ dians-domasna3/src/main/java/com/example/diansdomasna3/DiansDomasna3Application.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class DiansDomasna2Application {
+public class DiansDomasna3Application {
 
     public static void main(String[] args) {
-        SpringApplication.run(DiansDomasna2Application.class, args);
+        SpringApplication.run(DiansDomasna3Application.class, args);
     }
 
 }
